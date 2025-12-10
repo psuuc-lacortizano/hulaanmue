e533b7f4fe23bd3a9b368d815b6f819f5b75585e
psuuc-lacortizano <132413532+psuuc-lacortizano@users.noreply.github.com>
2025-12-11 01:00:48 +0800

    Delete mongoose directory

--- mongoose/routes/protected.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const requireLogin = require("../middleware/requiredLogin");
-
-router.get("/dashboard", requireLogin, (req, res) => {
-  res.render("dashboard");
-});
-
-module.exports = router;
